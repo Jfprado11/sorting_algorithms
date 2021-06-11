cc53406646f967c67f742a3dd1a6d1b70c8031f5
Mateo Londoño <mateolondono.u@gmail.com>
2021-06-11 16:17:44 -0500

    Uploading empty files from task 1 including header file

---
+++ 0-bubble_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array of integers in ascending
+ * order using the Bubble sort algorithm
+ * @size: size of the array
+ * @array: array to be sorted
+ * Return: a pointer to the newly created hash table.
+ */
+void bubble_sort(int *array, size_t size)
+{
+
+}
