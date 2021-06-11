dc8544bcf992274c9e1651a48fae83c1dcb7ff73
Mateo Londoño <mateolondono.u@gmail.com>
2021-06-11 17:08:02 -0500

    Uploading task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,5 +9,25 @@
  */
 void bubble_sort(int *array, size_t size)
 {
+	unsigned int i, j = 1;
+	int temp = 0;
 
+	if (size >= 2)
+	{
+	while (j > 0)
+	{
+		j = 0;
+		for (i = 0; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i + 1];
+				array[i + 1] = array[i];
+				array[i] = temp;
+				print_array(array, size);
+				j++;
+			}
+		}
+	}
+	}
 }
