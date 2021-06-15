6dfbcc8a77f62ecb0ccfc4957cb6a238f0100bd7
Felipe Prado <jfpc11@misena.edu.co>
2021-06-15 11:34:02 -0500

    adding the new insertion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,5 +1,4 @@
 #include "sort.h"
-
 /**
  * insertion_sort_list - sorts a doubly linked list of integers
  * in ascending order using the Insertion sort algorithm
@@ -20,22 +19,46 @@ void insertion_sort_list(listint_t **list)
 	next = sorted;
 	while (next != NULL)
 	{
-		while (sorted->n < current->n && sorted->prev != NULL)
+		sorted = next;
+		current = sorted->prev;
+		while (sorted->prev != NULL && sorted->n < current->n)
 		{
 			_swap(&sorted, &current, &(*list));
+			print_list(*list);
 		}
+		next = next->next;
 	}
 }
 
-
 /**
  * _swap - swap two nodes
- * @temp: temporal pointer to node
+ * @sorted: temporal pointer to node
  * @current: temporal pointer to node
  * @list: head pointer to list
  * Return: nothing
  */
-void _swap(listint_t **temp, listint_t **current, listint_t **list)
+void _swap(listint_t **sorted, listint_t **current, listint_t **list)
 {
 
+	if ((*current)->prev == NULL)
+	{
+		(*current)->prev = *sorted;
+		(*current)->next = (*sorted)->next;
+		(*sorted)->next = *current;
+		(*current)->next->prev = *current;
+		(*sorted)->prev = NULL;
+		*list = *sorted;
+		*current = (*sorted)->prev;
+	}
+	else
+	{
+		(*sorted)->prev = (*current)->prev;
+		(*current)->next = (*sorted)->next;
+		(*sorted)->next = *current;
+		(*current)->prev = *sorted;
+		if ((*current)->next != NULL)
+			(*current)->next->prev = *current;
+		(*sorted)->prev->next = *sorted;
+		*current = (*sorted)->prev;
+	}
 }
